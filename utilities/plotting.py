5435e187d201dd5c1a25a96030ca9e5aa6f7ddc8
stiven.lopez <Stiven.lopez2>
2021-05-08 12:18:45 -0500

    [ADD] funciones para construir graficas

--- utilities/plotting.py
+++ utilities/plotting.py
@@ -22,3 +22,18 @@ def plot_imputations(df: pd.DataFrame, column: str, method: str = 'median', **kw
     else:
         sns.distplot(df[column].fillna(df[column].fillna(df[column].mode())), **kwargs)
     return plt.show()
+
+
+def boxplot(data: pd.DataFrame, column: str, label: str = None, **kwargs):
+    if label is None:
+        return sns.catplot(data=data, y=column, kind='box', **kwargs)
+    else:
+        return sns.catplot(data=data, x=column, y=label, kind='box', **kwargs)
+
+
+def histogram(data: pd.DataFrame, column: str, **kwargs):
+    return sns.distplot(a=data[column], **kwargs)
+
+
+def scatterplot(data: pd.DataFrame, x: str, y: str, **kwargs):
+    return sns.relplot(x=x, y=y, data=data, kind="scatter", **kwargs)
