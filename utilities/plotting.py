05928f688ef4f1a5654ba2d7726aeeea0324febb
stiven.lopez <Stiven.lopez2>
2021-05-14 17:45:41 -0500

    [UPDATE] funcion para hacer grafico de barras

--- utilities/plotting.py
+++ utilities/plotting.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -37,3 +38,23 @@ def histogram(data: pd.DataFrame, column: str, **kwargs):
 
 def scatterplot(data: pd.DataFrame, x: str, y: str, **kwargs):
     return sns.relplot(x=x, y=y, data=data, kind="scatter", **kwargs)
+
+
+def barplot(data: pd.DataFrame, column: str):
+    """
+    Take a dataframe and graph the barchart of the variable of interest
+    :param data: dataframe
+    :param column: variable of interest
+    :return:
+    """
+    aux = data[column].value_counts().sort_values(ascending=True)
+    bars = tuple(aux.index.tolist())
+    values = aux.values.tolist()
+    y_pos = np.arange(len(bars))
+    colors = ['lightblue'] * len(bars)
+    colors[-1] = 'blue'
+    plt.figure(figsize=(12, 8))
+    plt.barh(y_pos, values, color=colors)
+    plt.title(f'{column} bar chart')
+    plt.yticks(y_pos, bars)
+    return plt.show()
